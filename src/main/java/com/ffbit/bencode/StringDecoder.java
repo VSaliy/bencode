a74918d2569b672647dad236f77ab7cb348da08b
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-01-11 14:06:50 +0200

    Made use of the common String separator for String decoder and encoder.

--- src/main/java/com/ffbit/bencode/StringDecoder.java
+++ src/main/java/com/ffbit/bencode/StringDecoder.java
@@ -1,10 +1,12 @@
 package com.ffbit.bencode;
 
+import static com.ffbit.bencode.StringEncoder.SEPARATOR;
+
 public class StringDecoder implements Decoder<String> {
 
     @Override
     public String decode(String input) {
-        int separatorIndex = input.indexOf(":");
+        int separatorIndex = input.indexOf(SEPARATOR);
         int length = Integer.valueOf(input.substring(0, separatorIndex));
         String value = input.substring(separatorIndex + 1, separatorIndex + 1 + length);
 
--- src/main/java/com/ffbit/bencode/StringEncoder.java
+++ src/main/java/com/ffbit/bencode/StringEncoder.java
@@ -1,11 +1,12 @@
 package com.ffbit.bencode;
 
 public class StringEncoder implements Encoder<String> {
+    public static final String SEPARATOR = ":";
 
     @Override
     public String encode(String input) {
         String str = String.valueOf(input);
-        return str.length() + ":" + str;
+        return str.length() + SEPARATOR + str;
     }
 
 }
