bfc5c03b2d06910728278d259c561a76e09d21d0
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-01-11 12:56:33 +0200

    Made use of the common prefix and suffix for Integer encoder and decoder.

--- src/main/java/com/ffbit/bencode/IntegerDecoder.java
+++ src/main/java/com/ffbit/bencode/IntegerDecoder.java
@@ -1,10 +1,13 @@
 package com.ffbit.bencode;
 
+import static com.ffbit.bencode.IntegerEncoder.PREFIX;
+import static com.ffbit.bencode.IntegerEncoder.SUFFIX;
+
 public class IntegerDecoder implements Decoder {
 
     @Override
     public Integer decode(String input) {
-        String number = input.substring(1, input.length() - 1);
+        String number = input.substring(PREFIX.length(), input.length() - SUFFIX.length());
         return Integer.valueOf(number);
     }
 
--- src/main/java/com/ffbit/bencode/IntegerEncoder.java
+++ src/main/java/com/ffbit/bencode/IntegerEncoder.java
@@ -1,10 +1,12 @@
 package com.ffbit.bencode;
 
 public class IntegerEncoder implements Encoder {
+    public static final String PREFIX = "i";
+    public static final String SUFFIX = "e";
 
     @Override
     public String encode(int input) {
-        return "i" + input + "e";
+        return PREFIX + input + SUFFIX;
     }
 
 }
