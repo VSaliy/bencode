caab6b9adcfc0479f86fee56e2e8f4a499e5d601
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-01-12 14:46:57 +0200

    String decoder reads from a stream.

--- src/main/java/com/ffbit/bencode/StringDecoder.java
+++ src/main/java/com/ffbit/bencode/StringDecoder.java
@@ -1,8 +1,22 @@
 package com.ffbit.bencode;
 
-import static com.ffbit.bencode.StringEncoder.SEPARATOR;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class StringDecoder implements Decoder<String> {
+    private final char SEPARATOR = ':';
+
+    private InputStream in;
+    private char current;
+    private int length;
+
+    @Deprecated
+    public StringDecoder() {
+    }
+
+    public StringDecoder(InputStream in) {
+        this.in = in;
+    }
 
     @Override
     public String decode(String input) {
@@ -11,6 +25,33 @@ public class StringDecoder implements Decoder<String> {
         String value = input.substring(separatorIndex + 1, separatorIndex + 1 + length);
 
         return value;
+    }
+
+    public String decode() throws IOException {
+        readLength();
+        checkSeparator();
+        byte[] bytes = new byte[length];
+        in.read(bytes);
+
+        return new String(bytes);
+    }
+
+    private void readLength() throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        while (Character.isDigit(read())) {
+            sb.append(current);
+        }
+
+        length = Integer.valueOf(sb.toString());
+    }
+
+    private char read() throws IOException {
+        current = (char) in.read();
+        return current;
+    }
+
+    private void checkSeparator() {
     }
 
 }
--- src/test/java/com/ffbit/bencode/StringDecoderTest.java
+++ src/test/java/com/ffbit/bencode/StringDecoderTest.java
@@ -5,6 +5,9 @@ import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -17,10 +20,23 @@ public class StringDecoderTest {
             "3:foo, foo",
             "3:fooi5e, foo"
     })
-    public void itShouldDecodeStrings(String input, String expectedOutput) throws Exception {
+    public void itShouldDecodeStringsOld(String input, String expectedOutput) throws Exception {
         Decoder decoder = new StringDecoder();
 
         assertThat(decoder.decode(input), is((Object) expectedOutput));
+    }
+
+    @Test
+    @Parameters({
+            "1:a, a",
+            "3:foo, foo",
+            "3:fooi5e, foo"
+    })
+    public void itShouldDecodeStrings(String input, String expected) throws Exception {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        StringDecoder decoder = new StringDecoder(in);
+
+        assertThat(decoder.decode(), is(expected));
     }
 
 }
