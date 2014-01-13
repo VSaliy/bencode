586cf019d31b9cda3cf99512d80276cfbaa20f00
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-01-13 20:31:01 +0200

    Started a deprecation campaign on decoders.

--- src/main/java/com/ffbit/bencode/Decoder.java
+++ src/main/java/com/ffbit/bencode/Decoder.java
@@ -1,7 +1,12 @@
 package com.ffbit.bencode;
 
+import java.io.IOException;
+
 public interface Decoder<T> {
 
+    @Deprecated
     T decode(String input);
+
+    T decode() throws IOException;
 
 }
