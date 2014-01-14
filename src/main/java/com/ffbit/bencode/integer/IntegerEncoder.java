4c288d21eb5b55ef8ed30103b80a94d3f6f2b3c3
Dmitriy Chizhikov <dmytro.chyzhykov@yandex.ru>
2014-01-14 16:59:09 +0200

    Integer encoder works with output stream.

--- src/main/java/com/ffbit/bencode/integer/IntegerEncoder.java
+++ src/main/java/com/ffbit/bencode/integer/IntegerEncoder.java
@@ -2,13 +2,35 @@ package com.ffbit.bencode.integer;
 
 import com.ffbit.bencode.Encoder;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class IntegerEncoder implements Encoder<Integer> {
     public static final String PREFIX = "i";
     public static final String SUFFIX = "e";
 
+    private OutputStream out;
+
+    public IntegerEncoder() {
+    }
+
+    public IntegerEncoder(OutputStream out) {
+        this.out = out;
+    }
+
     @Override
     public String encode(Integer input) {
-        return PREFIX + input + SUFFIX;
+        String result = PREFIX + input + SUFFIX;
+
+        if (out != null) {
+            try {
+                out.write(result.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
     }
 
 }
--- src/test/java/com/ffbit/bencode/integer/IntegerEncoderTest.java
+++ src/test/java/com/ffbit/bencode/integer/IntegerEncoderTest.java
@@ -1,11 +1,13 @@
 package com.ffbit.bencode.integer;
 
 import com.ffbit.bencode.Encoder;
-import com.ffbit.bencode.integer.IntegerEncoder;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -20,10 +22,25 @@ public class IntegerEncoderTest {
             "-1, i-1e",
             "42, i42e"
     })
-    public void itShouldEncodeIntegers(int input, String expectedOutput) throws Exception {
+    public void itShouldEncodeIntegersOld(Integer input, String expectedOutput) throws Exception {
         Encoder encoder = new IntegerEncoder();
 
         assertThat(encoder.encode(input), is(expectedOutput));
+    }
+
+    @Test
+    @Parameters({
+            "0, i0e",
+            "1, i1e",
+            "-1, i-1e",
+            "42, i42e"
+    })
+    public void itShouldEncodeIntegers(Integer input, String expectedOutput) throws Exception {
+        OutputStream out = new ByteArrayOutputStream();
+        Encoder encoder = new IntegerEncoder(out);
+
+        encoder.encode(input);
+        assertThat(out.toString(), is(expectedOutput));
     }
 
 }
