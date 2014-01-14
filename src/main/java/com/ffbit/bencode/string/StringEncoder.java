00ca737f5156a10386692ebee0deb04338f04847
Dmitriy Chizhikov <dmytro.chyzhykov@yandex.ru>
2014-01-14 17:30:26 +0200

    String encoder works with output stream.

--- src/main/java/com/ffbit/bencode/string/StringEncoder.java
+++ src/main/java/com/ffbit/bencode/string/StringEncoder.java
@@ -2,13 +2,41 @@ package com.ffbit.bencode.string;
 
 import com.ffbit.bencode.Encoder;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+
 public class StringEncoder implements Encoder<String> {
-    public static final String SEPARATOR = ":";
+    public static final char SEPARATOR = ':';
+
+    private OutputStream out;
+    private Charset charset;
+
+    public StringEncoder() {
+    }
+
+    public StringEncoder(OutputStream out, Charset charset) {
+        this.out = out;
+        this.charset = charset;
+    }
 
     @Override
     public String encode(String input) {
-        String str = String.valueOf(input);
-        return str.length() + SEPARATOR + str;
+        if (charset == null) {
+            return String.valueOf(input.length()) + SEPARATOR + input;
+        }
+
+        byte[] content = input.getBytes(charset);
+        String prefix = String.valueOf(content.length) + SEPARATOR;
+
+        try {
+            out.write(prefix.getBytes(charset));
+            out.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
 }
--- src/test/java/com/ffbit/bencode/string/StringEncoderTest.java
+++ src/test/java/com/ffbit/bencode/string/StringEncoderTest.java
@@ -1,10 +1,14 @@
 package com.ffbit.bencode.string;
 
+import com.ffbit.bencode.Decoder;
 import com.ffbit.bencode.Encoder;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.Charset;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -12,15 +16,30 @@ import static org.junit.Assert.assertThat;
 @RunWith(JUnitParamsRunner.class)
 public class StringEncoderTest {
 
+    private Charset charset = Decoder.DEFAULT_CHARSET;
+
+    @Test
+    @Parameters({
+            "a, 1:a",
+            "foo, 3:foo"
+    })
+    public void itShouldEncodeStringsOld(String input, String expectedOutput) throws Exception {
+        Encoder encoder = new StringEncoder();
+
+        assertThat(encoder.encode(input), is(expectedOutput));
+    }
+
     @Test
     @Parameters({
             "a, 1:a",
             "foo, 3:foo"
     })
     public void itShouldEncodeStrings(String input, String expectedOutput) throws Exception {
-        Encoder encoder = new StringEncoder();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Encoder encoder = new StringEncoder(out, charset);
 
-        assertThat(encoder.encode(input), is(expectedOutput));
+        encoder.encode(input);
+        assertThat(out.toString(charset.name()), is(expectedOutput));
     }
 
 }
